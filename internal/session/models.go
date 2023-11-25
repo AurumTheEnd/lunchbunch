653e7c227e7fc111a637cd936514bcabd33ad6f1
Ondřej Hrdlička <xhrdlic3@fi.muni.cz>
2023-11-25 23:56:06 +0100

    refactor: renamed session.Data.Authenticated to IsAuthenticated

--- internal/session/models.go
+++ internal/session/models.go
@@ -4,6 +4,6 @@ const CookieName = "lunchbunch_auth"
 const AuthenticationStoreKey = "authenticated"
 
 type Data struct {
-	Authenticated bool
-	Username      string
+	IsAuthenticated bool
+	Username        string
 }
