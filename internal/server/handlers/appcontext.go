61fd1a5f5684c642ff74c0eac195b6657afc427e
Ondřej Hrdlička <xhrdlic3@fi.muni.cz>
2023-11-25 23:58:05 +0100

    fix!: made cookies work

--- internal/server/handlers/appcontext.go
+++ internal/server/handlers/appcontext.go
@@ -15,53 +15,55 @@ type AppContext struct {
 }
 
 func (app *AppContext) LoginCookie(username string, req *http.Request, w http.ResponseWriter) (*session.Data, error) {
-	var s, getError = app.CookieStore.Get(req, session.CookieName)
-	if getError != nil {
-		return nil, getError
-	}
+	var s, _ = app.CookieStore.Get(req, session.CookieName)
+	// ignoring error because Get generates a new session
+	// if getError != nil {
+	// 	 return nil, getError
+	// }
 
-	var data = session.Data{
-		Authenticated: true,
-		Username:      username,
+	var data = &session.Data{
+		IsAuthenticated: true,
+		Username:        username,
 	}
 
 	s.Values[session.AuthenticationStoreKey] = data
 
 	if saveErr := s.Save(req, w); saveErr != nil {
-		return nil, saveErr
+		return &session.Data{IsAuthenticated: false}, saveErr
 	}
 
-	return &data, nil
+	return data, nil
 }
 
 func (app *AppContext) LogoutCookie(req *http.Request, w http.ResponseWriter) (*session.Data, error) {
-	var s, getError = app.CookieStore.Get(req, session.CookieName)
-	if getError != nil {
-		return nil, getError
-	}
+	var s, _ = app.CookieStore.Get(req, session.CookieName)
+	// ignoring error because Get generates a new session
+	// if getError != nil {
+	// 	 return nil, getError
+	// }
 
-	var data = session.Data{
-		Authenticated: false,
-	}
+	var data = &session.Data{IsAuthenticated: false}
 
 	s.Values[session.AuthenticationStoreKey] = data
+	s.Options.MaxAge = -1
 
 	if saveErr := s.Save(req, w); saveErr != nil {
-		return nil, saveErr
+		return data, saveErr
 	}
 
-	return &data, nil
+	return data, nil
 }
 
 func (app *AppContext) IsAuthenticated(req *http.Request) (bool, error) {
-	var s, getError = app.CookieStore.Get(req, session.CookieName)
-	if getError != nil {
-		return false, getError
-	}
+	var s, _ = app.CookieStore.Get(req, session.CookieName)
+	// ignoring error because Get generates a new session
+	// if getError != nil {
+	// 	 return nil, getError
+	// }
 
 	var userData, ok = s.Values[session.AuthenticationStoreKey].(session.Data)
 
-	return !ok || !userData.Authenticated, nil
+	return !ok || !userData.IsAuthenticated, nil
 }
 
 func (app *AppContext) UserData(req *http.Request) *session.Data {
@@ -71,10 +73,13 @@ func (app *AppContext) UserData(req *http.Request) *session.Data {
 	// 	 return nil, getError
 	// }
 
-	var userData, ok = s.Values[session.AuthenticationStoreKey].(session.Data)
+	var raw = s.Values[session.AuthenticationStoreKey]
+
+	var userData, ok = raw.(*session.Data)
 	if !ok {
-		return &session.Data{}
+		// data is mangled, maybe using old cookie format, lets revoke login
+		return &session.Data{IsAuthenticated: false}
 	}
 
-	return &userData
+	return userData
 }
