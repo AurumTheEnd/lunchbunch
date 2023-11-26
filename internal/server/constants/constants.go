6c27da0a7e166dedf51efbeffab9d8c1f5b6f1c6
Ondřej Hrdlička <xhrdlic3@fi.muni.cz>
2023-11-26 16:11:42 +0100

    feat: added "constant" for /poll/%d resolution

--- internal/server/constants/constants.go
+++ internal/server/constants/constants.go
@@ -1,7 +1,15 @@
 package constants
 
+import "fmt"
+
 const IndexPath = "/"
 const RegisterFormPath = "/register"
 const LoginFormPath = "/login"
 const LogoutPath = "/logout"
 const NewPollFormPath = "/poll/new"
+
+const pollPath = "/poll/%d"
+
+func PollPath(id uint) string {
+	return fmt.Sprintf(pollPath, id)
+}
