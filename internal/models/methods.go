4cc08a4bd4c134b94c5e5219cd25d08883a2222e
Ondřej Hrdlička <xhrdlic3@fi.muni.cz>
2023-11-26 23:41:12 +0100

    feat: introduced interface for frontend-facing helper methods

--- internal/models/methods.go
+++ internal/models/methods.go
@@ -2,18 +2,35 @@ package models
 
 import "time"
 
+type VoteCount interface {
+	TotalVotes() (total uint)
+	TotalVotesString() string
+}
+
 func (receiver RestaurantSnapshot) TotalVotes() (total uint) {
 	total = 0
 
 	for _, restaurant := range receiver.Restaurants {
-		total += uint(len(restaurant.Votes))
+		total += restaurant.TotalVotes()
 	}
 
 	return total
 }
 
+func (receiver Restaurant) TotalVotes() (total uint) {
+	return uint(len(receiver.Votes))
+}
+
 func (receiver RestaurantSnapshot) TotalVotesString() string {
-	if receiver.TotalVotes() == 1 {
+	return totalVotesString(receiver.TotalVotes())
+}
+
+func (receiver Restaurant) TotalVotesString() string {
+	return totalVotesString(receiver.TotalVotes())
+}
+
+func totalVotesString(count uint) string {
+	if count == 1 {
 		return "vote"
 	}
 
